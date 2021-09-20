ebb8b1cb039df920b16677ba62f60ff66deed110
Cubby Christopher Skeete <cskeete@esri.com>
2021-09-20 13:42:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -654,7 +654,7 @@ require({
 			  {
                 name: "launch",
                 type: "date"
-              ,
+	      },
 			  {
                 name: "name",
                 type: "string"
